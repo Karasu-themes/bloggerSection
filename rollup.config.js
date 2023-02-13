7ca1dbff2bd44843ac69328d7c4fdcde587b3c80
marceloTLD <marcelo.tld.snchez@gmail.com>
2023-02-12 23:27:49 -0300

    Agregado los archivos package y rollup.config

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { babel } from '@rollup/plugin-babel';
+import { minify } from 'rollup-plugin-esbuild'
+import rollupPluginLicense from 'rollup-plugin-license';
+
+const bannerLicense = `/*!
+* blogger.section 1.0.0
+* https://github.com/Karasu-themes/bloggerSection
+*
+* Copyright © 2022 MarceloTLD
+* 
+* Released under the MIT License
+*-----------------------------------------------*/`;
+export default {
+  plugins: [
+    nodeResolve(),
+    babel({ babelHelpers: 'bundled' }),
+    minify(),
+    rollupPluginLicense({ banner: bannerLicense })
+  ],
+  input: 'src/index.js',
+  output: {
+    file: './dist/bundle.js',
+    format: 'iife',
+    name: "BloggerSection"
+  }
+};
