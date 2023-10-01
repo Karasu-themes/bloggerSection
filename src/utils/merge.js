713b30f840aa6974d5c876153957be1c9613421b
MarceloTLD <marcelo.tld.snchez@gmail.com>
2023-10-01 17:02:28 -0300

    Se han eliminado archivos innecesarios actualmente

--- src/utils/merge.js
+++
@@ -1,9 +0,0 @@
-export function merge (source, properties) {
-	var property;
-	for (property in properties) {
-		if (properties.hasOwnProperty(property)) {
-			source[property] = properties[property];
-		}
-	}
-	return source;
-}
