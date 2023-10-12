a340c7d824b8a1cb9c5562b830791e571c642c16
MarceloTLD <marcelo.tld.snchez@gmail.com>
2023-10-12 17:16:16 -0300

    v1.1.0

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import { obs } from "./utils/intersectionObserver";
-import { getPosts } from "./utils/getPosts";
-import { merge } from "./utils/merge";
-import { parser, template } from 'blogger-utils/src/index';
+import start from "./utils/start";
 
-function init ( config, modes ) {
+function init() {
   const NODE_WRAPPER = document.querySelectorAll('.bs-wrapper');
 
   if (NODE_WRAPPER.length == 0) {
@@ -11,60 +8,13 @@ function init ( config, modes ) {
     return;
   }
 
-  // Default config
-  const DEFAULT_CONFIG = {
-    summary: 96,
-    results: 6,
-    empty: "Empty"
-  };
-
-  let intersectionObserver = obs((data, observe) => {
-    data.forEach((entry) => {
-      let node = entry.target;
-      const CONTAINER = node.querySelector('.bs-container');
-
-      // Stop if CONTAINER is null
-      if (!CONTAINER) return;
-
-      const _NODE_TPL = CONTAINER.querySelector('.bs-render'),
-      _STRING_TPL = _NODE_TPL.innerHTML.replace(/data-/g, '');
-
-      if (entry.isIntersecting) {
-        const CFG = merge({...DEFAULT_CONFIG, ...config, ...(modes ?? {})}, node.dataset ?? {}),
-        _DEFAULT_URI = window.location.protocol + '//' + window.location.hostname;
-
-        getPosts( CFG.homeUrl || _DEFAULT_URI, CFG.label, "bsFn" + (node.index+1), CFG.results )
-        .then(entry => {
-          let TEMPLATE = '';
-          
-          if (node.querySelector('.bs-loader')) node.querySelector('.bs-loader').innerHTML='';
-
-          if (entry.length > 0) {
-            entry.forEach(post => {
-              const _DATA = parser(post, merge(DEFAULT_CONFIG, CFG));
-              TEMPLATE+=template(_STRING_TPL, _DATA)
-            })
-          } else {
-            TEMPLATE+=`<p class="bs-empty">${CFG.empty || DEFAULT_CONFIG.empty}</p>`;
-          }
-
-          CONTAINER.innerHTML=TEMPLATE;
-          
-          // Modes
-          if (CFG.modes && CFG.modes[CFG.mode] && typeof CFG.modes[CFG.mode] == "function") {
-            CFG.modes[CFG.mode].call(this, node, CONTAINER, CFG);
-          }
-
-        })
-        
-        // unobserve
-        intersectionObserver.unobserve(node);
-      }
-    })
-  })
+  let intersectionObserver = new IntersectionObserver(start, {
+    threshold: 0.3,
+    rootMargin: "200px"
+  });
 
   NODE_WRAPPER.forEach((el, i) => {
-    el.index=i;
+    el.index = i;
     intersectionObserver.observe(el);
   })
 
---
+++ src/utils/start.js
@@ -0,0 +1,46 @@
+import { getPosts } from "./getPosts";
+import { parser, parserConfig, template } from 'blogger-utils/src/index';
+
+export default function start(data, observer) {
+    data.forEach((entry) => {
+        let node = entry.target;
+        const CONTAINER = node.querySelector('.bs-container');
+
+        // Stop if CONTAINER is null
+        if (!CONTAINER) return;
+
+        // Get the html template
+        const { innerHTML: STR_TEMPLATE } = node.querySelector('.bs-render');
+
+        if (entry.isIntersecting) {
+            const config = { ...node.dataset };
+
+            getPosts(config.homeUrl || (window.location.protocol + '//' + window.location.hostname), config.label ?? "", "bsFn" + (node.index + 1), config.results ?? 8)
+                .then(entry => {
+                    let TEMPLATE = '';
+                    if (node.querySelector('.bs-loader')) node.querySelector('.bs-loader').innerHTML = '';
+
+                    if (entry.length > 0) {
+                        entry.forEach(post => {
+                            const _DATA = parser(post, config);
+                            TEMPLATE += template(STR_TEMPLATE.replace(/data-/g, ''), _DATA)
+                        })
+                    } else {
+                        TEMPLATE += `<p class="bs-empty">${config.empty || "No posts founded"}</p>`;
+                    }
+
+                    CONTAINER.innerHTML = TEMPLATE;
+
+                    // initialize hook
+                    if (typeof window[config.hook] == "function" && config.hook) {
+                        window[config.hook].call(this, node, CONTAINER, config)
+                    }
+                })
+
+            // unobserve
+            observer.unobserve(node);
+
+        }
+
+    })
+}
